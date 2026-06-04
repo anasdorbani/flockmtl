500f383fa7ead046998e6bd5ee26e085d4aa088d
Anas Dorbani <anasdorbani@gmail.com>
2026-06-04 13:27:45 -0400

    Add flock_demo default model config entry

--- src/core/config/model.cpp
+++ src/core/config/model.cpp
@@ -18,15 +18,16 @@ void Config::SetupDefaultModelsConfig(duckdb::Connection& con, std::string& sche
                                  schema_name, table_name));
 
     con.Query(duckdb_fmt::format(
-            "INSERT OR IGNORE INTO {}.{} (model_name, model, provider_name) "
+            "INSERT OR IGNORE INTO {}.{} (model_name, model, provider_name, model_args) "
             "VALUES "
-            "('default', 'gpt-4o-mini', 'openai'), "
-            "('gpt-4o-mini', 'gpt-4o-mini', 'openai'), "
-            "('gpt-4o', 'gpt-4o', 'openai'), "
-            "('gpt-4o-transcribe', 'gpt-4o-transcribe', 'openai'),"
-            "('gpt-4o-mini-transcribe', 'gpt-4o-mini-transcribe', 'openai'),"
-            "('text-embedding-3-large', 'text-embedding-3-large', 'openai'), "
-            "('text-embedding-3-small', 'text-embedding-3-small', 'openai');",
+            "('default', 'gpt-4o-mini', 'openai', '{{}}'), "
+            "('gpt-4o-mini', 'gpt-4o-mini', 'openai', '{{}}'), "
+            "('gpt-4o', 'gpt-4o', 'openai', '{{}}'), "
+            "('gpt-4o-transcribe', 'gpt-4o-transcribe', 'openai', '{{}}'),"
+            "('gpt-4o-mini-transcribe', 'gpt-4o-mini-transcribe', 'openai', '{{}}'),"
+            "('text-embedding-3-large', 'text-embedding-3-large', 'openai', '{{}}'), "
+            "('text-embedding-3-small', 'text-embedding-3-small', 'openai', '{{}}'), "
+            "('flock_demo', 'demo', 'demo', '{{\"tuple_format\":\"JSON\",\"batch_size\":16,\"max_async_calls\":20,\"model_parameters\":{{}}}}');",
             schema_name, table_name));
 }
 
